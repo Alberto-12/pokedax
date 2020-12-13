40a530d2b2df7428b68ee5cda424e5cdc8d3f0d3
Alberto Serafini <ilgumo@gmai.com>
2020-12-13 12:56:09 -0600

    pokemonArray

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
-alert("Hello World")
-let food = "apple"
-document.write(food);
+let pokemonList =[
+    {name: 'balbasure', height: 7, types:['grass','poison']},
+    {name: 'Arbok', height: 3.5 , types:['fire','eletric']},
+    {name: 'beedrill',height: 1, types:['psychic','flying']},
+];    
+
+    
