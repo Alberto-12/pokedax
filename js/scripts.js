bff1fb3fe35e618870060d80195ba07b7dae7291
Alberto Serafini <ilgumo@gmai.com>
2020-12-17 12:52:21 -0600

    pokemonList for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,19 @@
-let pokemonList =[
-    {name: 'balbasure', height: 7, types:['grass','poison']},
-    {name: 'Arbok', height: 3.5 , types:['fire','eletric']},
-    {name: 'beedrill',height: 1, types:['psychic','flying']},
-];    
+let pokemonList = [
+  { name: "balbasure", height: 7, types: ["grass", "poison"] },
+  { name: "Arbok", height: 3.5, types: ["fire", "eletric"] },
+  { name: "beedrill", height: 1, types: ["psychic", "flying"] },
+];
 
-    
+for (let i = 0; i < pokemonList.length; i++) {
+  document.write(
+      '<div class= "box">'+
+      pokemonList[i].name +
+     "  (height:" +
+      pokemonList[i].height+
+      ")"+
+      "<br>"+
+      pokemonList[i].types+
+      "<br>"+
+      "</div>");
+
+}
