4850d6a69301ed86a2d60b32a553bf56342930a7
Alberto Serafini <ilgumo@gmai.com>
2020-12-20 12:42:17 -0600

    Implemented if statment

--- js/scripts.js
+++ js/scripts.js
@@ -5,15 +5,38 @@ let pokemonList = [
 ];
 
 for (let i = 0; i < pokemonList.length; i++) {
-  document.write(
-      '<div class= "box">'+
-      pokemonList[i].name +
-     "  (height:" +
-      pokemonList[i].height+
-      ")"+
-      "<br>"+
-      pokemonList[i].types+
-      "<br>"+
-      "</div>");
 
+  let color = ""
+  for(let k = 0; k < pokemonList[i].types.length; k++){
+    if(pokemonList[i].types[k] == "grass"){
+      color= '<span style="color:green;"> ';
+    }else if(pokemonList[i].types[k] == "fire"){
+      color= '<span style="color:red;"> ';
+    }else if(pokemonList[i].types[k] == "flying"){
+      color= '<span style="color:lightblue;"> ';
+    }
+  }
+
+  let size = ""
+  if (pokemonList[i].height > 5){
+    size = "WOw! It's a big Pokemon!"
+  }else if(pokemonList[i].height < 3){
+    size = "This is a small Pokemon!"
+  }else{
+    size = "It's a medium Pokemon!"
+  }
+  document.write(
+    '<div class= "box">' +
+      pokemonList[i].name +
+      "  (height:" +
+      pokemonList[i].height +
+      ")" +
+      "<br>" +
+      size +
+      color + 
+      pokemonList[i].types +
+      "<br>" +
+      "</div>"
+  );
 }
+
